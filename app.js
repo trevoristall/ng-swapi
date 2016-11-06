8ff31a6e7a1f5951bcdcfc2783e53666658255bd
Trevor Allen <trevoristall@gmail.com>
2016-11-06 13:35:02 -0500

    gh pages is being weird with https :hankey:

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = angular.module('app', []);
 
 // CONTROLLERS
 app.controller('mainController', ['$scope', '$http', function ($scope, $http) {
+
 	$http.get('https://swapi.co/api/people')
 		.success(function(result) {
 			$scope.people = result.results;
